69df153937f93b99f0872e893effc60724a5c939
Michal Kosinski <kosinski.mich@gmail.com>
2022-10-16 21:12:20 +0200

    request_dimension tests WIP; fixed DimensionRange bug

--- inst/WIP.R
+++ inst/WIP.R
@@ -4,94 +4,253 @@ dgs4_auth(path = Sys.getenv("G_SERVICE_ACCOUNT"),
           cache = F)
 
 # create test spreadsheet
-test_spreadsheet <- Spreadsheet(
-  properties = SpreadsheetProperties(),
-  sheets = Sheet(SheetProperties(sheetId = 0,
-                                 gridProperties = GridProperties(10,10)))
-)
-
-created <- send_create_req(test_spreadsheet)
-ss_id <- googledrive::as_id(created$spreadsheetId)
-
-req_min <- UpdateBordersRequest(
-        range = GridRange(0, 1, 10, 2, 9),
-        top = Border("DOTTED")
+created <- send_create_req(
+  spreadsheet = Spreadsheet(
+    sheets = Sheet(
+      properties = SheetProperties(
+        sheetId = 0, title = "Test_Sheet",
+        gridProperties = GridProperties(
+          10, 10
+        )
       )
+    )
+  )
+)
 
-resp_min <- send_batchUpdate_req(
+columnReq <- UpdateDimensionPropertiesRequest(
+  properties = DimensionProperties(pixelSize = 140,
+                                   hiddenByUser = TRUE),
+  range = DimensionRange(sheetId = 0,
+                         dimension = "COLUMNS",
+                         startIndex = 5,
+                         endIndex = 7),
+  fields = c("hiddenByUser", "pixelSize")
+)
+
+rowReq <- UpdateDimensionPropertiesRequest(
+  properties = DimensionProperties(
+    hiddenByUser = TRUE,
+    pixelSize = 50
+  ),
+  range = DimensionRange(sheetId = 0,
+                         dimension = "ROWS",
+                         startIndex = 7),
+  fields = c("hiddenByUser", "pixelSize")
+)
+
+update_col_resp <- send_batchUpdate_req(
   created$spreadsheetId,
-  req_min
+  columnReq
 )
 
-confirm <- send_get_req(created$spreadsheetId,
-                        fields = "sheets.data.rowData.values.userEnteredFormat.borders")
-
-top_borders_row_1 <- sapply(
-  confirm$sheets[[1]]$data[[1]]$rowData[[1]]$values,
-  \(x) if (!is.null(x$userEnteredFormat$borders))
-    x$userEnteredFormat$borders$top$style
-  else NA)
-
-top_borders_row_2 <- sapply(
-  confirm$sheets[[1]]$data[[1]]$rowData[[2]]$values,
-  \(x) if (!is.null(x$userEnteredFormat$borders))
-    x$userEnteredFormat$borders$top$style
-  else NA)
-
-expect_true(length(top_borders_row_1) == 0)
-expect_equal(top_borders_row_2,
-             c(rep(NA, 2), rep("DOTTED", 7)))
-
-req_max <- UpdateBordersRequest(
-  range = GridRange(0, 1, 10, 2, 9),
-  top = Border("NONE"),
-  bottom = Border("DOUBLE"),
-  left = Border("SOLID_MEDIUM"),
-  right = Border("SOLID_THICK"),
-  innerHorizontal = Border("DASHED"),
-  innerVertical = Border(colorStyle = ColorStyle("ACCENT3"))
-)
-
-resp_max <- send_batchUpdate_req(
+update_row_resp <- send_batchUpdate_req(
   created$spreadsheetId,
-  req_max
+  rowReq
 )
 
-confirm <- send_get_req(created$spreadsheetId,
-                        fields = "sheets.data.rowData.values.userEnteredFormat.borders")
+confirm <- send_get_req(
+  created$spreadsheetId,
+  fields = c("sheets.data.columnMetadata,sheets.data.rowMetadata"))
 
-top_borders_row_2 <- sapply(
-  confirm$sheets[[1]]$data[[1]]$rowData[[2]]$values,
-  \(x) if (!is.null(x$userEnteredFormat$borders$top$style))
-    x$userEnteredFormat$borders$top$style
-  else NA)
+col_properties <- vapply(confirm$sheets[[1]]$data[[1]]$columnMetadata,
+                         \(x) list(hiddenByUser = x$hiddenByUser,
+                                   pixelSize = x$pixelSize),
+                         vector("list", 2))
 
-top_borders_row_3 <- sapply(
-  confirm$sheets[[1]]$data[[1]]$rowData[[3]]$values,
-  \(x) if (!is.null(x$userEnteredFormat$borders))
-    x$userEnteredFormat$borders$top$style
-  else NA)
+row_properties <- vapply(confirm$sheets[[1]]$data[[1]]$rowMetadata,
+                         \(x) list(hiddenByUser = x$hiddenByUser,
+                                   pixelSize = x$pixelSize),
+                         vector("list", 2))
 
-left_borders_row_3 <- sapply(
-  confirm$sheets[[1]]$data[[1]]$rowData[[3]]$values,
-  \(x) if (!is.null(x$userEnteredFormat$borders))
-    x$userEnteredFormat$borders$left$style
-  else NA)
+expect_true(
+  all(as.logical(col_properties["hiddenByUser", c(6:7)]))
+)
 
-bottom_borders_row_10 <- sapply(
-  confirm$sheets[[1]]$data[[1]]$rowData[[10]]$values,
-  \(x) if (!is.null(x$userEnteredFormat$borders))
-    x$userEnteredFormat$borders$bottom$style
-  else NA)
+expect_true(
+  all(vapply(col_properties["hiddenByUser", c(1:5, 8:10)], is.null, logical(1)))
+)
 
-expect_true(all(vapply(top_borders_row_2, is.na, logical(1))))
-expect_equal(top_borders_row_3,
-             c(rep(NA, 2), rep("DASHED", 7)))
-expect_equal(left_borders_row_3,
-             c(rep(NA, 2), "SOLID_MEDIUM", rep("SOLID", 6)))
-expect_equal(bottom_borders_row_10,
-             c(rep(NA, 2), rep("DOUBLE", 7)))
+expect_equal(
+  as.numeric(col_properties["pixelSize", ]),
+  c(rep(100, 5), rep(140, 2), rep(100, 3))
+)
+
+expect_true(
+  all(as.logical(row_properties["hiddenByUser", c(8:10)]))
+)
+
+expect_true(
+  all(vapply(row_properties["hiddenByUser", c(1:7)], is.null, logical(1)))
+)
+
+expect_equal(
+  as.numeric(row_properties["pixelSize", ]),
+  c(rep(21, 7), rep(50, 3))
+)
 
 
+expect_failure(
+  expect_error(
+    columnReq <- InsertDimensionRequest(
+      range = DimensionRange(sheetId = 0,
+                             dimension = "COLUMNS",
+                             startIndex = 7,
+                             endIndex = 9),
+      inheritFromBefore = TRUE
+    )
+  )
+)
 
-googledrive::drive_trash(ss_id)
+expect_true(is.deepgsheets4Req(columnReq))
+
+expect_failure(
+  expect_error(
+    rowReq <- InsertDimensionRequest(
+      range = DimensionRange(sheetId = 0,
+                             dimension = "ROWS",
+                             startIndex = 7,
+                             endIndex = 9)
+    )
+  )
+)
+
+expect_true(is.deepgsheets4Req(rowReq))
+
+expect_failure(
+  expect_error(
+    insert_col_resp <- send_batchUpdate_req(
+      created$spreadsheetId,
+      columnReq
+    )
+  )
+)
+
+expect_failure(
+  expect_error(
+    insert_row_resp <- send_batchUpdate_req(
+      created$spreadsheetId,
+      rowReq
+    )
+  )
+)
+
+confirm <- send_get_req(
+  created$spreadsheetId,
+  fields = c("sheets.data.columnMetadata,sheets.data.rowMetadata"))
+
+col_properties <- vapply(confirm$sheets[[1]]$data[[1]]$columnMetadata,
+                         \(x) list(hiddenByUser = x$hiddenByUser,
+                                   pixelSize = x$pixelSize),
+                         vector("list", 2))
+
+row_properties <- vapply(confirm$sheets[[1]]$data[[1]]$rowMetadata,
+                         \(x) list(hiddenByUser = x$hiddenByUser,
+                                   pixelSize = x$pixelSize),
+                         vector("list", 2))
+
+expect_equal(
+  as.numeric(col_properties["pixelSize", 8:9]),
+  rep(140, 2)
+)
+
+expect_equal(
+  as.numeric(row_properties["pixelSize", 8:9]),
+  rep(50, 2)
+)
+
+#### lol ####
+
+expect_failure(
+  expect_error(
+    columnReq <- AppendDimensionRequest(
+      sheetId = 0,
+      length = 10,
+      dimension = "COLUMNS"
+    )
+  )
+)
+
+expect_true(is.deepgsheets4Req(columnReq))
+
+expect_failure(
+  expect_error(
+    rowReq <- AppendDimensionRequest(
+      sheetId = 0,
+      length = 20,
+      dimension = "ROWS"
+    )
+  )
+)
+
+expect_true(is.deepgsheets4Req(rowReq))
+
+expect_failure(
+  expect_error(
+    append_col_resp <- send_batchUpdate_req(
+      created$spreadsheetId,
+      columnReq
+    )
+  )
+)
+
+expect_failure(
+  expect_error(
+    append_row_resp <- send_batchUpdate_req(
+      created$spreadsheetId,
+      rowReq
+    )
+  )
+)
+
+confirm <- send_get_req(
+  created$spreadsheetId,
+  fields = c("sheets.properties.gridProperties"))
+
+expect_equal(c(rowCount = confirm$sheets[[1]]$properties$gridProperties$rowCount,
+               columnCount = confirm$sheets[[1]]$properties$gridProperties$columnCount),
+             c(rowCount = 32,
+               columnCount = 22))
+
+#### AutoResize
+
+expect_failure(
+  expect_error(
+    columnReq <- AutoResizeDimensionsRequest(
+      dimensions = DimensionRange(0,
+                                  "COLUMNS",
+                                  3, 8)
+    )
+  )
+)
+
+expect_true(is.deepgsheets4Req(columnReq))
+
+expect_failure(
+  expect_error(
+    rowReq <- AutoResizeDimensionsRequest(
+      dimensions = DimensionRange(0,
+                                  "ROWS",
+                                  17, 20)
+    )
+  )
+)
+
+expect_true(is.deepgsheets4Req(rowReq))
+
+expect_failure(
+  expect_error(
+    autoresize_col_resp <- send_batchUpdate_req(
+      created$spreadsheetId,
+      columnReq
+    )
+  )
+)
+
+expect_failure(
+  expect_error(
+    autoresize_row_resp <- send_batchUpdate_req(
+      created$spreadsheetId,
+      rowReq
+    )
+  )
+)
